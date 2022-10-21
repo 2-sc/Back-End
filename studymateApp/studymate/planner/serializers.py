dd665308f5aa1e309723dd77856c58c81fccaa8a
ryukyunghye <ryukyung123@naver.com>
2022-10-22 02:25:29 +0900

    :sparkles:(feature/ryu/#11): 플래너 수정 및 삭제 기능

--- studymateApp/studymate/planner/serializers.py
+++ studymateApp/studymate/planner/serializers.py
@@ -1,26 +1,80 @@
 from rest_framework import serializers
 
-from .models import Todo, Schedule, Comment
+from user.models import User
+from .models import Todo as TodoModel
+from .models import Schedule as ScheduleModel
+from .models import Comment as CommentModel
 
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Todo
+        model = TodoModel
         fields = ('user_id', 'todo', 'complete', 'register')
+
+    def create(self, validated_data):
+        todo = TodoModel.objects.create(
+            user_id=User.objects.get(pk=self.user_id),
+            todo=validated_data['todo'],
+            complete=validated_data['complete'],
+            register=validated_data['register']
+        )
+        todo.save()
+        return todo
+
+
+class TodoDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoModel
+        fields = ('complete',)
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Schedule
+        model = ScheduleModel
         fields = ('schedule', 'register')
+
+    def create(self, validated_data):
+        schedule = ScheduleModel.objects.create(
+            user_id=User.objects.get(pk=self.user_id),
+            schedule=validated_data['schedule'],
+            register=validated_data['register']
+        )
+        schedule.save()
+        return schedule
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Comment
-        fields = ('comment', 'register')
+        model = CommentModel
+        fields = ('user_id', 'comment', 'register')
 
-    def validate(self, data):
-        if Comment.objects.filter(register=data['comment']).exists():
-            raise serializers.ValidationError({'register': '오늘의 다짐이 존재합니다.'})
-        return data
+    def create(self, validated_data):
+        comment = CommentModel.objects.create(
+            user_id=User.objects.get(pk=self.user_id),
+            comment=validated_data['comment'],
+            register=validated_data['register']
+        )
+        comment.save()
+        return comment
+
+
+class CommentDetailSerializer(serializers.ModelSerializer):
+    comment = serializers.CharField(required=True)
+
+    class Meta:
+        model = CommentModel
+        fields = ("comment",)
+
+
+class LogCommentSerializer(serializers.ModelSerializer):
+    temp_todo = TodoSerializer(many=True)
+
+    class Meta:
+        model = CommentModel
+        fields = ('user_id', 'comment', 'register', 'temp_todo',)
+
+
+class LogTotalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoModel
+        fields = ('todo', 'complete',)
--- studymateApp/studymate/planner/views.py
+++ studymateApp/studymate/planner/views.py
@@ -1,87 +1,127 @@
-from rest_framework import status
+import jwt
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
-from .models import Todo, Schedule, Comment
-from .serializers import TodoSerializer, ScheduleSerializer, CommentSerializer
-
 from datetime import datetime
+
+from .models import Schedule, Comment
+from .models import Todo as TodoModel
+from .serializers import TodoSerializer, ScheduleSerializer, CommentSerializer, TodoDetailSerializer, \
+    CommentDetailSerializer
+from .permissions import isAuthenticated
+from studymate.settings import SECRET_KEY
+from user.models import User
 
 
 # Create your views here.
 
 # 투두리스트만 조회
 class TodoAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
     def get(self, request):
-        if request.data.get("user_id") is not None:
-            # request -> {"user_id": Int, "register": null}
+        token = request.headers['Authorization']
+        user_token = jwt.decode(token, SECRET_KEY, algorithms='HS256')
+        # request -> {"user_id": Int, "register": null}
+        try:
             if request.data.get("register") is None:
-                todo = Todo.objects.filter(register=datetime.now().strftime('%Y-%m-%d'), user_id=request.data.get("user_id"))
+                todo = TodoModel.objects.filter(register=datetime.now().strftime('%Y-%m-%d'),
+                                                # user_id=request.data.get("user_id"))
+                                                user_id=user_token['id'])
                 serializer = TodoSerializer(todo, many=True)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             # request -> {"user_id": Int, "register": "0000-00-00"}
             elif request.data.get("register") == "0000-00-00":
-                todo = Todo.objects.order_by('-register').filter(user_id=request.data.get("user_id"))  # 내림차순
+                todo = TodoModel.objects.order_by('-register').filter(user_id=user_token['id'])  # 내림차순
                 serializer = TodoSerializer(todo, many=True)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             # request -> {"user_id": Int, "register": "date(yyyy-mm-dd)"}
             else:
-                todo = Todo.objects.filter(register=request.data.get("register"), user_id=request.data.get("user_id"))
+                todo = TodoModel.objects.filter(register=request.data.get("register"), user_id=user_token['id'])
                 serializer = TodoSerializer(todo, many=True)
                 return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response({'isSuccess': False, 'msg':'투두 조회를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({'isSuccess': False, 'msg': '투두 조회를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 # 투두리스트 생성
 class TodoCreateAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
     def post(self, request):
+        token = request.headers['Authorization']
+        user_token = jwt.decode(token, SECRET_KEY, algorithms='HS256')
         serializer = TodoSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.user_id = user_token['id']
             serializer.save()
             return Response({'isSuccess': True, 'msg': '투두 생성 되었습니다.'}, status=status.HTTP_201_CREATED)
         return Response({'isSuccess': False, 'msg': '투두 생성을 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-# 투두리스트 삭제
+# 투두리스트 수정 및 삭제
 class TodoDetailAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
+    def patch(self, request, pk):
+        user_token = request.headers['Authorization']
+        user_decode = jwt.decode(user_token, SECRET_KEY, algorithms='HS256')
+        todo = get_object_or_404(TodoModel, pk=pk)
+        serializer = TodoDetailSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk):
-        todo = get_object_or_404(Todo, pk=pk)
-        todo.delete()
-        if todo is not None:
+        try:
+            todo = TodoModel.objects.get(pk=pk)
+            todo.delete()
             return Response({'isSuccess': True, 'msg': '투두 삭제 되었습니다'}, status=status.HTTP_202_ACCEPTED)
-        return Response({'isSuccess': False, 'msg': '투두 삭제를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({'isSuccess': False, 'msg': '투두 삭제를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 스케줄 조회
 class ScheduleAPIView(APIView):
-
-
+    permission_classes = [isAuthenticated]
 
     def get(self, request):
-        id = request.data.get("user_id")
-        # request -> {"user_id": Int, "register": null}
-        id = request.data.get("user_id")
-        if request.data.get("register") is None:
-            schedule = Schedule.objects.filter(register=datetime.now().strftime("%Y-%m-%d"), user_id=id)
-            serializer = ScheduleSerializer(schedule, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        # request -> {"user_id": Int, "register": "0000-00-00"}
-        elif request.data.get("register") == "0000-00-00":
-            schedule = Schedule.objects.filter(user_id=id).order_by('-register')
-            serializer = ScheduleSerializer(schedule, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        # request -> {"user_id": Int, "register": "date(yyyy-mm-dd)"}
-        else:
-            schedule = Schedule.objects.filter(register=request.data.get("register"), user_id=request.data.get("user_id"))
-            serializer = ScheduleSerializer(schedule, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        token = request.headers['Authorization']
+        user_token = jwt.decode(token, SECRET_KEY, algorithms='HS256')
+        try:
+            # request -> {"user_id": Int, "register": null}
+            if request.data.get("register") is None:
+                schedule = Schedule.objects.filter(register=datetime.now().strftime("%Y-%m-%d"),
+                                                   user_id=request.data.get("user_id"))
+                serializer = ScheduleSerializer(schedule, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            # request -> {"user_id": Int, "register": "0000-00-00"}
+            elif request.data.get("register") == "0000-00-00":
+                schedule = Schedule.objects.filter(user_id=request.data.get("user_id")).order_by('-register')
+                serializer = ScheduleSerializer(schedule, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            # request -> {"user_id": Int, "register": "date(yyyy-mm-dd)"}
+            else:
+                schedule = Schedule.objects.filter(register=request.data.get("register"),
+                                                   user_id=request.data.get("user_id"))
+                serializer = ScheduleSerializer(schedule, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response({'isSuccess': False, 'msg': '스케줄 조회를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 스케줄 생성
 class ScheduleCreateAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
     def post(self, request):
+        token = request.headers['Authorization']
+        user_token = jwt.decode(token, SECRET_KEY, algorithms='HS256')
         serializer = ScheduleSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.user_id = user_token['id']
             serializer.save()
             return Response({'isSuccess': True, 'msg': '스케줄 생성 되었습니다.'}, status=status.HTTP_201_CREATED)
         return Response({'isSuccess': False, 'msg': '스케줄 생성을 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
@@ -89,41 +129,58 @@ class ScheduleCreateAPIView(APIView):
 
 # 스케줄 삭제
 class ScheduleDetailAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
     def delete(self, request, pk):
-        schedule = get_object_or_404(Schedule, pk=pk)
-        schedule.delete()
-        if schedule is not None:
+        try:
+            schedule = Schedule.objects.get(pk=pk)
+            schedule.delete()
             return Response({'isSuccess': True, 'msg': '스케줄 삭제 되었습니다.'}, status=status.HTTP_202_ACCEPTED)
-        return Response({'isSuccess': False, 'msg': '스케줄 삭제를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({'isSuccess': False, 'msg': '스케줄 삭제를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 다짐 조회
 class CommentAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
     def get(self, request):
-        id = request.data.get("user_id")
-        # request -> {"user_id": Int, "register": null}
-        if request.data.get("comment") is None:
-            today = datetime.now().strftime("%Y-%m-%d")
-            comment = Comment.objects.filter(register=today)
-            serializer = CommentSerializer(comment, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        # request -> {"user_id": Int, "register": "0000-00-00"}
-        elif request.data.get("register") == "0000-00-00":
-            comment = Comment.objects.all().order_by('-register')
-            serializer = CommentSerializer(comment, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        # request -> {"user_id": Int, "register": "date(yyyy-mm-dd)"}
-        else:
-            comment = Comment.objects.filter(register=request.data.get("register"))
-            serializer = CommentSerializer(comment, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        token = request.headers['Authorization']
+        user_token = jwt.decode(token, SECRET_KEY, algorithms='HS256')
+        try:
+            # request -> {"user_id": Int, "register": null} [0]
+            if request.data.get("register") is None:
+                comment = Comment.objects.filter(register=datetime.now().strftime("%Y-%m-%d"),
+                                                 user_id=user_token['id'])
+                serializer = CommentSerializer(comment, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            # request -> {"user_id": Int, "register": "0000-00-00"}
+            elif request.data.get("register") == "0000-00-00":
+                comment = Comment.objects.all().order_by('-register').filter(user_id=user_token['id'])
+                serializer = CommentSerializer(comment, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            # request -> {"user_id": Int, "register": "date(yyyy-mm-dd)"}
+            else:
+                comment = Comment.objects.filter(register=request.data.get("register"),
+                                                 user_id=user_token['id'])
+                serializer = CommentSerializer(comment, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response({'isSuccess': False, 'msg': '다짐 조회를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 다짐 생성
 class CommentCreateAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
     def post(self, request):
-        serializer = CommentSerializer(data=request.data)
+        token = request.headers['Authorization']
+        user_token = jwt.decode(token, SECRET_KEY, algorithms='HS256')
+        serializer = CommentDetailSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.user_id = user_token['id']
+            if Comment.objects.filter(comment=serializer.validated_data['comment']).exists():
+                return Response({'isSuccess': False, 'msg': '오늘의 다짐이 존재합니다.'}, status=status.HTTP_400_BAD_REQUEST)
             serializer.save()
             return Response({'isSuccess': True, 'msg': '다짐 생성 되었습니다.'}, status=status.HTTP_201_CREATED)
         return Response({'isSuccess': False, 'msg': '다짐 생성을 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
@@ -131,7 +188,10 @@ class CommentCreateAPIView(APIView):
 
 # 다짐 수정 및 삭제
 class CommentDetailAPIView(APIView):
+    permission_classes = [isAuthenticated]
+
     def patch(self, request, pk):
+        user_token = request.headers['Authorization']
         comment = get_object_or_404(Comment, pk=pk)
         serializer = CommentSerializer(comment, data=request.data)
         if serializer.is_valid():
@@ -140,8 +200,9 @@ class CommentDetailAPIView(APIView):
         return Response({'isSuccess': False, 'msg': '다짐 수정을 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        comment = get_object_or_404(Comment, pk=pk)
-        comment.delete()
-        if comment is not None:
+        try:
+            comment = Comment.objects.get(pk=pk)
+            comment.delete()
             return Response({'isSuccess': True, 'msg': '다짐 삭제 되었습니다.'}, status=status.HTTP_202_ACCEPTED)
-        return Response({'isSuccess': False, 'msg': '다짐 삭제를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({'isSuccess': False, 'msg': '다짐 삭제를 실패했습니다.'}, status=status.HTTP_400_BAD_REQUEST)
