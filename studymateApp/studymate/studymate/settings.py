fc46d80a3d2e95dc752428ec96d2426d2f294847
hsh_519 <suk000519@naver.com>
2022-10-23 19:07:35 +0900

    feature/hong/#14 recycle: 세팅파일 서버관련 코드수정

--- studymateApp/studymate/studymate/settings.py
+++ studymateApp/studymate/studymate/settings.py
@@ -26,9 +26,32 @@ SECRET_KEY = 'django-insecure-^7seyqe&*h)y^x5oxgrr(0qpxjw-x%fzr++-r#2edda413bca2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-54-209-170-145.compute-1.amazonaws.com', 'studymate.site']
+ALLOWED_HOSTS = [] #['ec2-54-209-170-145.compute-1.amazonaws.com', 'studymate.site']
 
+def is_ec2_linux():
+    if os.path.isfile("sys/hypervisor/uuid"):
+        with open("sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
 
+def get_linux_ec2_private_ip():
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
 # Application definition
 
 INSTALLED_APPS = [
